f73da03b1955fb25ccbbdd0928f010d6559b17ac
erdemelvan <erdemelvan35@gmail.com>
2023-07-17 16:52:56 +0300

    feat: Environment Setup

---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <Button size='default' variant='default' >Click Me</Button>
+    </div>
+  );
+}
