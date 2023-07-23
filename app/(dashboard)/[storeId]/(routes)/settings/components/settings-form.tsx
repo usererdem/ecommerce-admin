82c06337b2fcbe4822a05c21b8b667a1b117a138
erdemelvan <erdemelvan35@gmail.com>
2023-07-23 22:58:20 +0300

    feat: created form

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -10,6 +10,15 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { Heading } from "@/components/ui/heading";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
 interface SettingsFormProps {
   initialData: Store;
@@ -29,16 +38,52 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
     defaultValues: initialData,
   });
 
+  const onSubmit = async (data: SettingsFormValues) => {
+    console.log(data);
+  };
+
   return (
     <>
       <div className='flex items-center justify-between'>
         <Heading title='Settings' description='Manage store preferences' />
-        <Button variant='destructive' size='icon' onClick={() => {}}>
+        <Button
+          disabled={loading}
+          variant='destructive'
+          size='icon'
+          onClick={() => setOpen(true)}>
           {" "}
           <Trash className='h-4 w-4' />{" "}
         </Button>
       </div>
       <Separator />
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className='space-y-8 w-full'>
+          <div className='grid grid-cols-3 gap-8'>
+            <FormField
+              control={form.control}
+              name='name'
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={loading}
+                      placeholder='Store name'
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button disabled={loading} className='ml-auto' type='submit'>
+            Save changes
+          </Button>
+        </form>
+      </Form>
     </>
   );
 };
