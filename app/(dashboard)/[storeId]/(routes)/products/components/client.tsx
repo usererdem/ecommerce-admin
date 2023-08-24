8cbfc4206d72c784bbbddb124d564b03aee614d0
erdemelvan <erdemelvan35@gmail.com>
2023-08-24 16:21:21 +0300

    feat: made necessary variable name etc. changes from copied folder to make products page work

--- app/(dashboard)/[storeId]/(routes)/products/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/components/client.tsx
@@ -9,33 +9,33 @@ import { Separator } from "@/components/ui/separator";
 import { DataTable } from "@/components/ui/data-table";
 import { ApiList } from "@/components/ui/api-list";
 
-import { BillboardColumn, columns } from "./columns";
+import { ProductColumn, columns } from "./columns";
 
-interface BillboardClientProps {
-  data: BillboardColumn[];
+interface ProductClientProps {
+  data: ProductColumn[];
 }
 
-export const BillboardClient: React.FC<BillboardClientProps> = ({ data }) => {
+export const ProductClient: React.FC<ProductClientProps> = ({ data }) => {
   const router = useRouter();
   const params = useParams();
   return (
     <>
       <div className='flex items-center justify-between'>
         <Heading
-          title={`Billboards (${data.length})`}
-          description='Manage billboards for your store'
+          title={`Products (${data.length})`}
+          description='Manage products for your store'
         />
         <Button
-          onClick={() => router.push(`/${params.storeId}/billboards/new`)}>
+          onClick={() => router.push(`/${params.storeId}/products/new`)}>
           <Plus className='mr-2 h-4 w-4' />
           Add New
         </Button>
       </div>
       <Separator />
       <DataTable columns={columns} data={data} searchKey="label" />
-      <Heading title="API" description="API calls for Billboards" />
+      <Heading title="API" description="API calls for Products" />
       <Separator />
-      <ApiList entityName="billboards" entityIdName="billboardId" />
+      <ApiList entityName="products" entityIdName="productId" />
     </>
   );
 };
--- app/(dashboard)/[storeId]/(routes)/products/components/columns.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/components/columns.tsx
@@ -5,16 +5,54 @@ import { CellAction } from "./cell-action";
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
-export type BillboardColumn = {
+export type ProductColumn = {
   id: string;
-  label: string;
+  name: string;
+  price: string;
+  size: string;
+  color: string;
+  isFeatured: boolean;
+  isArchived: boolean;
   createdAt: string;
 };
 
-export const columns: ColumnDef<BillboardColumn>[] = [
+export const columns: ColumnDef<ProductColumn>[] = [
   {
-    accessorKey: "label",
-    header: "Label",
+    accessorKey: "name",
+    header: "Name",
+  },
+  {
+    accessorKey: "isArchived",
+    header: "Archived",
+  },
+  {
+    accessorKey: "isFeatured",
+    header: "Featured",
+  },
+  {
+    accessorKey: "price",
+    header: "Price",
+  },
+  {
+    accessorKey: "category",
+    header: "Category",
+  },
+  {
+    accessorKey: "size",
+    header: "Size",
+  },
+  {
+    accessorKey: "color",
+    header: "Color",
+    cell: ({ row }) => (
+      <div className='flex items-center gap-x-2'>
+        {row.original.color}
+        <div
+          className='h-6 w-6 rounded-full border'
+          style={{ backgroundColor: row.original.color }}
+        />
+      </div>
+    ),
   },
   {
     accessorKey: "createdAt",
